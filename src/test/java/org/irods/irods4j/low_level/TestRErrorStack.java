e55b5fff2ac40402a651e8dcdb51d9a78a65fe4f
Kory Draughn <korydraughn@ymail.com>
2025-01-22 09:15:29 -0500

    Add test demonstrating capture of RError information.

--- src/test/java/org/irods/irods4j/low_level/TestRErrorStack.java
+++ src/test/java/org/irods/irods4j/low_level/TestRErrorStack.java
@@ -1,36 +1,60 @@
 package org.irods.irods4j.low_level;
 
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Optional;
+
+import org.irods.irods4j.api.IRODSException;
+import org.irods.irods4j.common.JsonUtil;
+import org.irods.irods4j.common.XmlUtil;
+import org.irods.irods4j.high_level.connection.IRODSConnection;
+import org.irods.irods4j.high_level.connection.QualifiedUsername;
+import org.irods.irods4j.high_level.policy.IRODSRules;
+import org.irods.irods4j.high_level.policy.IRODSRules.RuleArguments;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestRErrorStack {
 
+	static String host = "localhost";
+	static int port = 1247;
+	static String zone = "tempZone";
+	static String username = "rods";
+	static String password = "rods";
+	static IRODSConnection conn;
+
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		XmlUtil.enablePrettyPrinting();
+		JsonUtil.enablePrettyPrinting();
+
+		conn = new IRODSConnection();
+		conn.connect(host, port, new QualifiedUsername(username, zone));
+		conn.authenticate("native", password);
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
-	}
+		conn.disconnect();
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
+		XmlUtil.disablePrettyPrinting();
+		JsonUtil.disablePrettyPrinting();
 	}
 
 	@Test
-	void testRErrorMessagesAreCapturedAndMadeAvailable() {
-		assumeTrue(false, "Disabled until test is implemented");
-		fail("Not yet implemented");
+	void testRcCommCapturesRErrorInfo() throws Exception {
+		var ruleArgs = new RuleArguments();
+		ruleArgs.ruleText = "writeLine('stdout', 'missing syntax";
+		ruleArgs.input = Optional.empty();
+		ruleArgs.output = Optional.empty();
+		ruleArgs.ruleEnginePluginInstance = Optional.of("irods_rule_engine_plugin-irods_rule_language-instance");
+
+		assertThrows(IRODSException.class, () -> IRODSRules.executeRule(conn.getRcComm(), ruleArgs));
+		assertEquals(conn.getRcComm().rError.count, 1);
+		assertTrue(conn.getRcComm().rError.RErrMsg_PI.get(0).msg.contains("parseRuleSet: error parsing rule."));
 	}
 
 }
