084f4c71f749cc501f7d0d3d35d48d3960cff106
Kory Draughn <korydraughn@ymail.com>
2026-07-29 14:34:55 -0400

    [#150] IRODSReplicas: Hex escape single quotes in logical paths

--- src/main/java/org/irods/irods4j/high_level/vfs/IRODSReplicas.java
+++ src/main/java/org/irods/irods4j/high_level/vfs/IRODSReplicas.java
@@ -63,8 +63,8 @@ public class IRODSReplicas {
 		throwIfNullOrEmpty(logicalPath, "Path is null or empty");
 		throwIfLessThanLowerBound(replicaNumber, 0, "Replica number is less than 0");
 
-		String collName = LogicalPath.parentPath(logicalPath);
-		String dataName = LogicalPath.objectName(logicalPath);
+		String collName = LogicalPath.singleQuotesToHex(LogicalPath.parentPath(logicalPath));
+		String dataName = LogicalPath.singleQuotesToHex(LogicalPath.objectName(logicalPath));
 
 		String query = "select DATA_SIZE where COLL_NAME = '%s' and DATA_NAME = '%s' and DATA_REPL_NUM = '%d'";
 		List<List<String>> rows = IRODSQuery.executeGenQuery2(comm, String.format(query, collName, dataName, replicaNumber));
@@ -95,8 +95,8 @@ public class IRODSReplicas {
 		throwIfNullOrEmpty(logicalPath, "Path is null or empty");
 		throwIfNullOrEmpty(leafResourceName, "Leaf resource is null or empty");
 
-		String collName = LogicalPath.parentPath(logicalPath);
-		String dataName = LogicalPath.objectName(logicalPath);
+		String collName = LogicalPath.singleQuotesToHex(LogicalPath.parentPath(logicalPath));
+		String dataName = LogicalPath.singleQuotesToHex(LogicalPath.objectName(logicalPath));
 
 		String query = "select DATA_SIZE where COLL_NAME = '%s' and DATA_NAME = '%s' and RESC_NAME = '%s'";
 		List<List<String>> rows = IRODSQuery.executeGenQuery2(comm, String.format(query, collName, dataName, leafResourceName));
@@ -712,8 +712,8 @@ public class IRODSReplicas {
 		throwIfNullOrEmpty(logicalPath, "Path is null or empty");
 		throwIfLessThanLowerBound(replicaNumber, 0, "Replica number is less than 0");
 
-		String collName = LogicalPath.parentPath(logicalPath);
-		String dataName = LogicalPath.objectName(logicalPath);
+		String collName = LogicalPath.singleQuotesToHex(LogicalPath.parentPath(logicalPath));
+		String dataName = LogicalPath.singleQuotesToHex(LogicalPath.objectName(logicalPath));
 
 		String query = "select DATA_MODIFY_TIME where COLL_NAME = '%s' and DATA_NAME = '%s' and DATA_REPL_NUM = '%d'";
 		List<List<String>> rows = IRODSQuery.executeGenQuery2(comm, String.format(query, collName, dataName, replicaNumber));
@@ -745,8 +745,8 @@ public class IRODSReplicas {
 		throwIfNullOrEmpty(logicalPath, "Path is null or empty");
 		throwIfNullOrEmpty(leafResourceName, "Resource is null or empty");
 
-		String collName = LogicalPath.parentPath(logicalPath);
-		String dataName = LogicalPath.objectName(logicalPath);
+		String collName = LogicalPath.singleQuotesToHex(LogicalPath.parentPath(logicalPath));
+		String dataName = LogicalPath.singleQuotesToHex(LogicalPath.objectName(logicalPath));
 
 		String query = "select DATA_MODIFY_TIME where COLL_NAME = '%s' and DATA_NAME = '%s' and RESC_NAME = '%s'";
 		List<List<String>> rows = IRODSQuery.executeGenQuery2(comm, String.format(query, collName, dataName, leafResourceName));
@@ -854,8 +854,8 @@ public class IRODSReplicas {
 		throwIfNullOrEmpty(logicalPath, "Path is null or empty");
 		throwIfNullOrEmpty(leafResourceName, "Resource is null or empty");
 
-		String collName = LogicalPath.parentPath(logicalPath);
-		String dataName = LogicalPath.objectName(logicalPath);
+		String collName = LogicalPath.singleQuotesToHex(LogicalPath.parentPath(logicalPath));
+		String dataName = LogicalPath.singleQuotesToHex(LogicalPath.objectName(logicalPath));
 
 		String query = "select DATA_REPL_NUM where COLL_NAME = '%s' and DATA_NAME = '%s' and RESC_NAME = '%s'";
 		List<List<String>> rows = IRODSQuery.executeGenQuery2(comm, String.format(query, collName, dataName, leafResourceName));
@@ -887,8 +887,8 @@ public class IRODSReplicas {
 		throwIfNullOrEmpty(logicalPath, "Path is null or empty");
 		throwIfLessThanLowerBound(replicaNumber, 0, "Replica number is less than 0");
 
-		String collName = LogicalPath.parentPath(logicalPath);
-		String dataName = LogicalPath.objectName(logicalPath);
+		String collName = LogicalPath.singleQuotesToHex(LogicalPath.parentPath(logicalPath));
+		String dataName = LogicalPath.singleQuotesToHex(LogicalPath.objectName(logicalPath));
 
 		String query = "select RESC_NAME where COLL_NAME = '%s' and DATA_NAME = '%s' and DATA_REPL_NUM = '%d'";
 		List<List<String>> rows = IRODSQuery.executeGenQuery2(comm, String.format(query, collName, dataName, replicaNumber));
@@ -920,8 +920,8 @@ public class IRODSReplicas {
 		throwIfNullOrEmpty(logicalPath, "Path is null or empty");
 		throwIfLessThanLowerBound(replicaNumber, 0, "Replica number is less than 0");
 
-		String collName = LogicalPath.parentPath(logicalPath);
-		String dataName = LogicalPath.objectName(logicalPath);
+		String collName = LogicalPath.singleQuotesToHex(LogicalPath.parentPath(logicalPath));
+		String dataName = LogicalPath.singleQuotesToHex(LogicalPath.objectName(logicalPath));
 
 		String query = "select DATA_ID where COLL_NAME = '%s' and DATA_NAME = '%s' and DATA_REPL_NUM = '%d'";
 		return !IRODSQuery.executeGenQuery2(comm, String.format(query, collName, dataName, replicaNumber)).isEmpty();
@@ -948,8 +948,8 @@ public class IRODSReplicas {
 		throwIfNullOrEmpty(logicalPath, "Path is null or empty");
 		throwIfNullOrEmpty(leafResourceName, "Resource is null or empty");
 
-		String collName = LogicalPath.parentPath(logicalPath);
-		String dataName = LogicalPath.objectName(logicalPath);
+		String collName = LogicalPath.singleQuotesToHex(LogicalPath.parentPath(logicalPath));
+		String dataName = LogicalPath.singleQuotesToHex(LogicalPath.objectName(logicalPath));
 
 		String query = "select DATA_ID where COLL_NAME = '%s' and DATA_NAME = '%s' and RESC_NAME = '%s'";
 		return !IRODSQuery.executeGenQuery2(comm, String.format(query, collName, dataName, leafResourceName)).isEmpty();
